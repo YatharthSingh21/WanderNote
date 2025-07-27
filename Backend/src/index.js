d33939bd36a3d78185c4687ed2211cce173dd3cc
YatharthSingh21 <yathartha.2015@gmail.com>
2025-07-28 01:56:07 +0530

    deployment commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,21 +4,37 @@ import dotenv from "dotenv";
 import {connectDB} from "./config/DBconnection.js";
 import cors from "cors";
 import rateLimiter from "./middlewares/ratelimiter.js"
+import path from 'path';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
 const PORT = 3001;
 const app = express();
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production"){
+    app.use(cors());
+}
+
 app.use(express.json())
 app.use("/api/notes", notesRouter);
 app.use(rateLimiter);
 
+console.log("__dirname:", __dirname);
+console.log("Resolved path:", path.join(__dirname, "../Frontend/dist"));
 
-connectDB().then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`Server is runnning on ${PORT}`)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+    app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
     });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
 
